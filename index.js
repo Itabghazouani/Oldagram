604e0e42c7f60cdf5f55ae54b0be9b5e1d9e327e
itab ghazouani <itab.ghazouani@gmail.com>
2024-01-16 11:40:05 +0100

    using JS to render three posts and css added

--- index.js
+++ index.js
@@ -28,3 +28,35 @@ const posts = [
     }
 ]
 
+const container = document.querySelector(".container")
+const imgEl = document.getElementById("img-el")
+
+function renderPosts() {
+  let html = ""
+  for (let i = 0; i < posts.length; i++) {
+    html += `
+    <section class="post">
+      <div class="poster">
+      <img src="${posts[i].avatar}" class="avatar">
+        <div class="user">
+          <h1 class="bold-text">${posts[i].name}</h1>
+          <h4>${posts[i].location}</h4>
+        </div>
+      </div>
+      <img src="${posts[i].post}" class="img-post" id="img-el">
+      <div class="icons">
+        <img src="images/icon-heart.png" alt="Heart Icon" class="icon">
+        <img src="images/icon-comment.png" alt="comment Icon" class="icon">
+        <img src="images/icon-dm.png" alt="dm Icon" class="icon">
+      </div>
+      <div class="caption">
+        <h3 class="bold-text">${posts[i].likes} likes</h3>
+        <p><span class="bold-text">${posts[i].username}</span>${posts[i].comment}</p>
+      </div>
+    </section>
+    `
+  }
+  container.innerHTML += html
+}
+
+renderPosts()
